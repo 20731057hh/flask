7e8afc225d5f4072823f5e43b8d1311080404b9d
20731057hh <20731057hh@gmail.com>
2019-09-08 11:17:10 +0900

    チュートリアルのstep4.1が完了
    Squashed commit:
    
    [cb15bf7] flask.extの古い呼び方をflask_scriptの新しい呼び方に修正
    
    [662167e] managerの対応
    
    [df8fbc4] Flask-Scriptの追加
    
    [a828ddc] ユーザクラスの追加

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,4 +1,42 @@
+from sqlalchemy.orm import synonym
+from werkzeug import check_password_hash, generate_password_hash
+
 from flaskr import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), default='', nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    _password = db.Column('password', db.String(100), nullable=False)
+
+    def _get_password(self):
+        return self._password
+
+    def _set_password(self, password):
+        if password:
+            password = password.strip() #空白を削除
+        self._get_password = generate_password_hash(password)
+    password_descriptor = property(_get_password, _set_password)
+    password = synonym('_password', descriptor=password_descriptor)
+
+    def check_password(self, password):
+        password = password.strip() #空白を削除
+        if not password:
+            return False
+        return check_password_hash(self.password, password)
+
+    @classmethod
+    def authenticate(cls, query, email, password):
+        user = query(cls).filter(cls.email==email).first()
+        if user is None:
+            return None, False
+        return user, user.check_password(password)
+
+    def __repr__(self):
+        return u'<User id={self.id} email={self.email!r}>'.format(
+                sefl=self)
 
 class Entry(db.Model):
     __tablename__ = 'entries'
--- manage.py
+++ manage.py
@@ -1,3 +1,14 @@
-from flaskr import app
+from __future__ import print_function
+# これだとエラーになるので下記に修正
+# from flask.ext.script import Manager
+from flask_script import Manager
+from flaskr import app, db
 
-app.run(host='127.0.0.1', port=5000, debug=True)
+manager = Manager(app)
+
+@manager.command
+def init_db():
+    db.create_all()
+
+if __name__ == '__main__':
+    manager.run()
