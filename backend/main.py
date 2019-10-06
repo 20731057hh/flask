dd4806172bfef007a97243bb45cefec0d6754bda
20731057hh <20731057hh@gmail.com>
2019-10-06 12:50:56 +0900

    FlaskでのVuejs表示

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from random import randint
 
-app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
+app = Flask(__name__,
+            static_folder='../frontend/dist/static',
+            template_folder='../frontend/dist')
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber':randint(1,100)
+    }
+    return jsonify(response)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
