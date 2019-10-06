091c7f5379c7b9a7d2eae13fc67bb1fbfa80d552
20731057hh <20731057hh@gmail.com>
2019-10-06 12:56:04 +0900

    frontendファイルの再取り込み完了

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>This is a home page</h1>
+<!-- VueCliのデフォルト処理
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+}
+</script>
+
+<!-- VueCliのデフォルト処理
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+-->
