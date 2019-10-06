cf3c2f3cd2a85f7bcb63dd6c76146207d41cafcc
20731057hh <20731057hh@gmail.com>
2019-10-06 16:49:37 +0900

    FlaskのRESTapiを追加

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template, jsonify
+from flask_restful import Api, Resource
 from random import randint
 
 app = Flask(__name__,
             static_folder='../frontend/dist/static',
             template_folder='../frontend/dist')
+api = Api(app)
 
-# @app.route('/api/random')
-# def random_number():
-#     response = {
-#         'randomNumber':randint(1,100)
-#     }
-#     return jsonify(response)
+class Spam(Resource):
+    def get(self):
+        return{'id': 42, 'name': 'Name'}
+
+api.add_resource(Spam, '/api/spam')
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber':randint(1,100)
+    }
+    return jsonify(response)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
