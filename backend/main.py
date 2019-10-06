c661dfc186f96fd7807ffbd68f303c7711481088
20731057hh <20731057hh@gmail.com>
2019-10-06 17:36:11 +0900

    Blueprintでapiファイルを分割

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,11 @@
 from flask import Flask, render_template, jsonify
-from flask_restful import Api, Resource
+from api import api_bp
 from random import randint
 
 app = Flask(__name__,
             static_folder='../frontend/dist/static',
             template_folder='../frontend/dist')
-api = Api(app)
-
-class Spam(Resource):
-    def get(self):
-        return{'id': 42, 'name': 'Name'}
-
-api.add_resource(Spam, '/api/spam')
+app.register_blueprint(api_bp)
 
 @app.route('/api/random')
 def random_number():
