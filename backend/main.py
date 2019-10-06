ab48cb96e024420001f5d6d7653070d781ce240a
20731057hh <20731057hh@gmail.com>
2019-10-06 19:25:31 +0900

    ORMでDB機能を追加

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, jsonify
 from api import api_bp
+from models import get_all, init_db, insert
 from random import randint
 
 app = Flask(__name__,
             static_folder='../frontend/dist/static',
             template_folder='../frontend/dist')
+app.config['SQLALCHEMY_DATBASE_URI'] = 'sqlite:///myspa.db'
 app.register_blueprint(api_bp)
 
 @app.route('/api/random')
@@ -20,4 +22,9 @@ def index(path):
     return render_template('index.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        init_db(app)
+        if not get_all():
+            insert('foo', 'This is foo')
+            insert('bar', 'This is bar')
     app.run()
