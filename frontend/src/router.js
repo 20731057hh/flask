2fda7cbc92ac1e4309a8d09525d62f659ce3e091
20731057hh <20731057hh@gmail.com>
2019-10-06 15:56:11 +0900

    testページを追加

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import Test from './views/Test.vue'
 
 Vue.use(Router)
 
@@ -22,7 +23,7 @@ export default new Router({
     {
       path: '/test',
       name: 'test',
-      component: Testd
+      component: Test
     }
   ]
 })
